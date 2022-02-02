6c495db15f620e18b83fcc34eb785d779a384edd
sunnatganiev <sunnatganiev09@gmail.com>
2022-02-02 17:43:16 +0500

    added cart model

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   return (
     <CartProvider>
       <Header />
+      <Cart />
       <main>
         <Meals />
       </main>
